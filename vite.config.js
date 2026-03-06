6702be8272f8384fafef0fea8bc3574b2e18052e
DhamodharThyiagarajan <dhamodcruz1@gmail.com>
2026-03-06 17:30:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/ResumeBuilder/',
+  base: '/',
   plugins: [react(),tailwindcss()],
 })
